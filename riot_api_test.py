d2620750f29fe2589776b1042b90024eb45877c6
Fazekas Dániel <fazdate15@gmail.com>
2021-03-30 20:18:48 +0200

    Kommentek

--- riot_api_test.py
+++ riot_api_test.py
@@ -1,8 +1,13 @@
 from riotwatcher import LolWatcher
 import sys
 
+# Az első parancssori argumentum lesz az API kulcs
 watcher = LolWatcher(sys.argv[1])
+
+# A másidik parancssori argumentum lesz a régió
 region = sys.argv[2]
+
+# A harmadik parancssori argumentum lesz a játékos neve
 me = watcher.summoner.by_name(region, sys.argv[3])
 stats = watcher.league.by_summoner(region, me['id'])[0]
 matches = watcher.match.matchlist_by_account(region, me['accountId'])
